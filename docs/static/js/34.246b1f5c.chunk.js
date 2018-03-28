4101a4db87af790fb09b90bdf58e53235f505583
Longyarnz <longyarnz@gmail.com>
2018-03-28 17:55:55 +0100

    Add docs/ folder to repo

---
+++ docs/static/js/34.246b1f5c.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([34],{61:function(e,a,t){"use strict";function n(e){var a=e.click,t=e.attr;return l.a.createElement("div",{className:t,onClick:a},l.a.createElement("a",null,l.a.createElement("main",null),l.a.createElement("i",{className:"material-icons"},"add_shopping_cart")))}Object.defineProperty(a,"__esModule",{value:!0}),a.default=n;var c=t(5),l=t.n(c)}});
+//# sourceMappingURL=34.246b1f5c.chunk.js.map
